4ae405405d9ab1d0d17dd777b52f4541f834cb0e
Lakorthus <julio28ve@gmail.com>
2022-09-24 15:25:08 +0100

    Refactoring with Documentation

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,60 +1,58 @@
 import { useState } from "react";
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
-};
-
-const Anecdote = (props) => {
-  return (
-    <div>
-      <p>{props.anecdote}</p>
-      <p>Has {props.votes} votes</p>
-    </div>
-  );
-};
+const Anecdote = ({ text, vote_count }) => (
+  <>
+    {text}
+    <div>has {vote_count} votes</div>
+  </>
+);
 
 const App = () => {
   const anecdotes = [
-    "If it hurts, do it more often",
+    "If it hurts, do it more often.",
     "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
     "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
     "Premature optimization is the root of all evil.",
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
   ];
 
   const [selected, setSelected] = useState(0);
-  const [vote, setVote] = useState(Array(anecdotes.length).fill(0));
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0]);
 
-  const handleVote = () => {
-    const voteArray = [...vote];
-    voteArray[selected] += 1;
-    setVote(voteArray);
+  let most_voted = 0;
+  for (let i = 0; i < anecdotes.length; i++) {
+    if (votes[i] > votes[most_voted]) {
+      most_voted = i;
+    }
+  }
+
+  const next = () => {
+    let newSelected = selected;
+    while (newSelected === selected) {
+      newSelected = Math.floor(Math.random() * anecdotes.length);
+    }
+
+    setSelected(newSelected);
   };
 
-  const getMostVoted = () => {
-    const maxNumber = Math.max(...vote);
-    const mostVoted = vote.indexOf(maxNumber);
-    return mostVoted;
+  const vote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
   };
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <Anecdote anecdote={anecdotes[selected]} votes={vote[selected]} />
-      <Button
-        onClick={() =>
-          setSelected(Math.floor(Math.random() * anecdotes.length))
-        }
-        text="Next Anecdote"
-      />
-      <Button onClick={handleVote} text="Vote" />
-      <h1>Anecdote with most votes</h1>
-      <Anecdote
-        anecdote={anecdotes[getMostVoted()]}
-        votes={vote[getMostVoted()]}
-      />
+      <h3>Anecdote of the day</h3>
+      <Anecdote text={anecdotes[selected]} vote_count={votes[selected]} />
+      <div>
+        <button onClick={vote}>vote</button>{" "}
+        <button onClick={next}>next anecdote</button>
+      </div>
+      <h3>Anecdote with most votes</h3>
+      <Anecdote text={anecdotes[most_voted]} vote_count={votes[most_voted]} />
     </div>
   );
 };
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,63 +1,63 @@
 import { useState } from "react";
 
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>;
-};
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
 
-const StadisticsLine = ({ text, value }) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + bad + neutral;
+
+  if (all === 0) {
+    return (
+      <>
+        <h3>statistics</h3>
+        <p>no feedback given</p>
+      </>
+    );
+  }
+
+  const average = (good - bad) / all;
+  const positive = 100 * (good / all);
+
   return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
+    <div>
+      <h3>statistics</h3>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + "%"} />
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-const Stadistics = (props) => {
-  const [goodCount, neutralCount, badCount] = props.feedback;
-  const allCount = goodCount + neutralCount + badCount;
-
-  if (allCount > 0) {
-    return (
-      <>
-        <StadisticsLine text="Good" value={goodCount} />
-        <StadisticsLine text="Neutral" value={neutralCount} />
-        <StadisticsLine text="Bad" value={badCount} />
-        <StadisticsLine text="All" value={allCount} />
-        <StadisticsLine
-          text="Average"
-          value={(goodCount - badCount) / allCount}
-        />
-        <StadisticsLine
-          text="Positive"
-          value={`${(goodCount * 100) / allCount} %`}
-        />
-      </>
-    );
-  } else {
-    return <StadisticsLine text="No Stadistics" />;
-  }
-};
+const Button = ({ handleClick, label }) => (
+  <button onClick={handleClick}>{label}</button>
+);
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   return (
-    <div>
-      <h1>Give FeedBack</h1>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h1>Stadistics</h1>
-      <table>
-        <tbody>
-          <Stadistics feedback={[good, neutral, bad]} />
-        </tbody>
-      </table>
-    </div>
+    <>
+      <h3>give feedback</h3>
+      <div>
+        <Button handleClick={() => setGood(good + 1)} label="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} label="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} label="bad" />
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
   );
 };
 
