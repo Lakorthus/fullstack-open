f5c89fc7b8b91dd5046ef1aff04de757c881ae0a
Lakorthus <julio28ve@gmail.com>
2022-09-24 01:32:23 +0100

    1.11 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,43 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const StadisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Stadistics = (props) => {
+  const [goodCount, neutralCount, badCount] = props.feedback;
+  const allCount = goodCount + neutralCount + badCount;
+
+  if (allCount > 0) {
+    return (
+      <>
+        <StadisticsLine text="Good" value={goodCount} />
+        <StadisticsLine text="Neutral" value={neutralCount} />
+        <StadisticsLine text="Bad" value={badCount} />
+        <StadisticsLine text="All" value={allCount} />
+        <StadisticsLine
+          text="Average"
+          value={(goodCount - badCount) / allCount}
+        />
+        <StadisticsLine
+          text="Positive"
+          value={`${(goodCount * 100) / allCount} %`}
+        />
+      </>
+    );
+  } else {
+    return <StadisticsLine text="No Stadistics" />;
+  }
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -9,24 +47,18 @@ const App = () => {
 
   return (
     <div>
-      <h2>Give feedback</h2>
-      <div>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
-      </div>
-    
-      <h2>Statistics</h2>
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral +bad}</p>
-        <p>average {(good + neutral +bad) / 3}</p>
-        <p>positive {(good/(good + neutral + bad))*100}%</p>
-      </div>
-
+      <h1>Give FeedBack</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>Stadistics</h1>
+      <table>
+        <tbody>
+          <Stadistics feedback={[good, neutral, bad]} />
+        </tbody>
+      </table>
     </div>
   );
 };
-export default App
+
+export default App;
